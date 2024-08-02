59621973a8a3c9c0523a220cbaff52a3b4cee858
Ahmed <ahmedkent62@gmail.com>
2024-08-02 07:40:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8000;
+
+// init app
+const app = express();
+
+// view engine
+app.set("view engine", "ejs");
+
+// listen server
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
